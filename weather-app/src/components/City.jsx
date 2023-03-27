5855152017732662312723192f2780a61e64d52d
elinoza <hillcakmak@gmail.com>
2023-03-27 13:55:47 +0300

    lang-lon being implemented

--- weather-app/src/components/City.jsx
+++ weather-app/src/components/City.jsx
@@ -14,28 +14,23 @@ import {
   Form,
 } from "react-bootstrap";
 
-
 export default function City(props) {
   const [data, setData] = useState([]);
   const [dataList, setDatalist] = useState([]);
   const [dataCurrent, setCurrent] = useState([]);
 
-
-  const [city, setCity] = useState("");
-  const triggerError=(mode)=>{
- 
-    props.triggerError(mode)
-
-  }
+  // const [geoCoo, setGeoCoo] = useState("");
+  const triggerError = (mode) => {
+    props.triggerError(mode);
+  };
 
   const getWeather = useCallback(async () => {
     try {
       console.log("getweather function heyyo");
       const url = process.env.REACT_APP_URL;
-      const key = process.env.REACT_APP_KEY;
-      let units="metric"
+      let geoCoo = props.geoCoo;
 
-      let query = `/api/${city}`;
+      let query = `/api/${geoCoo}`;
 
       let response = await fetch(url + query);
       if (response.ok) {
@@ -48,23 +43,21 @@ export default function City(props) {
         console.log("weather", weather);
         console.log("dataCurrent", data);
       } else {
-        console.log(response)
-      ;
+        console.log(response);
       }
     } catch (error) {
-      console.log("hey",error);
-      triggerError(true)
+      console.log("error", error);
+      triggerError(true);
     }
-  }, [city]);
-
+  }, [props.geoCoo]);
 
   //useEffect(()=>{getWeather()},[getWeather]) this is also working properly, I wonder why we didnt choose this version
 
-  useEffect(getWeather, [city, getWeather]);
+  useEffect(getWeather, [props.geoCoo, getWeather]);
 
-  useEffect(() => {
-    setCity(props.city);
-  }, [setCity, props.city]);
+  // useEffect(() => {
+  //   setGeoCoo(props.geoCoo);
+  // }, [setGeoCoo, props.geoCoo]);
 
   const getDay = (i) => {
     let days = [
@@ -85,74 +78,71 @@ export default function City(props) {
     return days[normalizedIndex];
   };
 
-  const fahToCelcius = (fah) => {
- 
-    return (fah-273,15)
+  const kelToCelcius = (kel) => {
+    return (kel - 273.15).toFixed(0);
   };
   let i = 0;
 
   return (
-  
     <>
-      {data ? <><div className="d-flex justify-content-center h-50 w-100">
-        
-        <Row className="mt-5 align-self-center">
-          {dataCurrent.weather && (
-            <Col lg={12} className="text-white  text-center">
-              <img
-                src={`http://openweathermap.org/img/w/${dataCurrent.weather[0].icon}.png`}
-                style={{ width: "60px" }}
-              />
-              <h3>
-                {data.name},{data.country}
-              </h3>
-              <h1>
-                {fahToCelcius(dataCurrent.main.temp)}{" "}
-               { '\u00B0'}C
-              </h1> 
-
-              <h3 className="">{dataCurrent.weather[0].description}</h3>
-              <h5>{getDay(0)}</h5>
-            </Col>
-          )}
-        </Row>
-      </div>
-      <div
-        className=" mt-5 days-report p-4 "
-     
-      >
-      
-        <Row className="mt-4 text-white text-center  ">
-          {dataList && dataList.length > 0 &&
-            dataList.map((data, index) => {
-              i = index % 8 === 1 ? i + 1 : i;
-
-              return index % 8 === 1 ? (
-                <Col
-                key={data.dt}
-                  xs={12}
-                  className="text-white m-2 d-flex  px-5 text-center"
-                >
-                  <h5 className="d-inline mr-auto   ">{ getDay(i)}</h5>
-
+      {data ? (
+        <>
+          <div className="d-flex justify-content-center h-50 w-100">
+            <Row className="mt-5 align-self-center">
+              {dataCurrent.weather && (
+                <Col lg={12} className="text-white  text-center">
                   <img
-                    src={`http://openweathermap.org/img/w/${data.weather[0].icon}.png`}
-                    style={{ width: "36px" }}
+                    src={`http://openweathermap.org/img/w/${dataCurrent.weather[0].icon}.png`}
+                    style={{ width: "60px" }}
                   />
+                  <h3>
+                    {data.name},{data.country}
+                  </h3>
+                  <h1>
+                    {kelToCelcius(dataCurrent.main.temp)} {"\u00B0"}C
+                  </h1>
 
-                  <h5 className="d-inline ml-5 ">
-                    {data.main.temp}
-                    { '\u00B0'}C
-                  
-                  </h5>
+                  <h3 className="">{dataCurrent.weather[0].description}</h3>
+                  <h5>{getDay(0)}</h5>
                 </Col>
-              ) : (
-              ""
-              );
-            })}
-        </Row>
-      </div> </>:""}
-     
+              )}
+            </Row>
+          </div>
+          <div className=" mt-5 days-report p-4 ">
+            <Row className="mt-4 text-white text-center  ">
+              {dataList &&
+                dataList.length > 0 &&
+                dataList.map((data, index) => {
+                  i = index % 8 === 1 ? i + 1 : i;
+
+                  return index % 8 === 1 ? (
+                    <Col
+                      key={data.dt}
+                      xs={12}
+                      className="text-white m-2 d-flex  px-5 text-center"
+                    >
+                      <h5 className="d-inline mr-auto   ">{getDay(i)}</h5>
+
+                      <img
+                        src={`http://openweathermap.org/img/w/${data.weather[0].icon}.png`}
+                        style={{ width: "36px" }}
+                      />
+
+                      <h5 className="d-inline ml-5 ">
+                        {kelToCelcius(data.main.temp)}
+                        {"\u00B0"}C
+                      </h5>
+                    </Col>
+                  ) : (
+                    ""
+                  );
+                })}
+            </Row>
+          </div>{" "}
+        </>
+      ) : (
+        ""
+      )}
     </>
   );
 }
--- weather-app/src/components/Main.jsx
+++ weather-app/src/components/Main.jsx
@@ -12,23 +12,46 @@ import {
   Button,
   Form,
 } from "react-bootstrap";
-import { MdFavorite, MdFavoriteBorder,MdSearch } from "react-icons/md";
-import {GiHamburgerMenu} from "react-icons/gi";
-
+import { MdFavorite, MdFavoriteBorder, MdSearch } from "react-icons/md";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 import CityApp from "./City";
 import Favs from "./Favs";
 
-
-
-
 function Main() {
   const [me, setMe] = useState([]);
-  const [city, setCity] = useState("TOKAT");
+  const [city, setCity] = useState("");
+  const [geoCoo, setGeoCoo] = useState("");
   const [temporaryCity, setTemp] = useState("");
   const [favCollection, setFavCollection] = useState(null);
   const [error, setError] = useState(false);
-  const[sideBarOpen,setSideBar]=useState(false);
+  const [sideBarOpen, setSideBar] = useState(false);
+
+//change city to geoLocation
+const convertToGeoLoc=async(selectedCity)=>{
+  try {
+    //const url = process.env.REACT_APP_URL;
+    const url="http://api.openweathermap.org/geo/1.0/direct?"
+    const limit=10;
+    const key= process.env.REACT_APP_KEY
+    let query= `q=${selectedCity}&limit=${limit}&appid=${key}`
+    let response = await fetch(url+query)
+    if (response.ok){
+      let cities= await response.json()
+      console.log(cities)
+    }
+    else{
+      console.log(response)
+    }
+
+   
+
+  } catch (error) {
+    console.log(error)
+    
+  }
+ };
+
 
   /// get Me  and setMe
   const getMe = useCallback(async () => {
@@ -60,11 +83,33 @@ function Main() {
   //componentDidMount -->
   useEffect(() => {
     getMe();
+    
+  }, []);
+
+  const getCurrentLocation=()=>{
+    if (navigator.geolocation) { 
+      console.log("hey from geo")
+    navigator.geolocation.getCurrentPosition((position)=> {
+      const p=position.coords;
+      console.log("coor",p,p.latitude,p.longitude); setGeoCoo(`lat=${p.latitude}&lon=${p.longitude}`) })
+     
+}
+else{
+  console.log("Geolocation is not supported by this browser.")
+}
+   
+  }
+
+  useEffect(() => {
+getCurrentLocation()
+
   }, []);
 
   const deleteFav = async (selectedCity) => {
     try {
-      let cityFound = favCollection.find((elem) => elem.favCity === selectedCity);
+      let cityFound = favCollection.find(
+        (elem) => elem.favCity === selectedCity
+      );
       console.log(cityFound._id);
       const url = process.env.REACT_APP_URL;
       // const id ="6038f14842ca86203e481354";
@@ -88,35 +133,34 @@ function Main() {
     }
   };
 
-
   const postFav = async () => {
-if(city && city!=""){  try {
-  const url = process.env.REACT_APP_URL;
-  // const id ="6038f14842ca86203e481354";
-  let query = `/users/${me._id}/favs`;
-  let response = await fetch(url + query, {
-    method: "POST",
-    headers: {
-      Authorization: `Bearer ${localStorage.getItem("token")}`,
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ favCity: city }),
+    if (city && city != "") {
+      try {
+        const url = process.env.REACT_APP_URL;
+        // const id ="6038f14842ca86203e481354";
+        let query = `/users/${me._id}/favs`;
+        let response = await fetch(url + query, {
+          method: "POST",
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ favCity: city }),
 
-    withCredentials: true, // use cookies
-  });
-  if (response.ok) {
-    console.log("fav is posted", city);
-    getMe();
-  } else {
-    console.log(response, city);
-  }
-} catch (error) {
-  console.log(error);
-}}
-else{
-  console.log("choose a city to ad to favorites")
-}
-  
+          withCredentials: true, // use cookies
+        });
+        if (response.ok) {
+          console.log("fav is posted", city);
+          getMe();
+        } else {
+          console.log(response, city);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    } else {
+      console.log("choose a city to ad to favorites");
+    }
   };
 
   // useEffect(postFav,[favCity,postFav])
@@ -130,91 +174,118 @@ else{
   //   }
   // }, [favCity, postFav]);
 
+
+  
   const keyUp = (e) => {
     setTemp(e.currentTarget.value);
-    
-
   };
-const toggleSideBar=()=>{setSideBar(!sideBarOpen)}
+  const toggleSideBar = () => {
+    setSideBar(!sideBarOpen);
+  };
 
-const submitForm = (e) => {
-  e.preventDefault();
-  let insensitiveCity = temporaryCity.toUpperCase();
-  setCity(insensitiveCity);
-};
-const  triggerError=(error)=>{
-setError(error)
+  const submitForm = (e) => {
+    e.preventDefault();
+    let insensitiveCity = temporaryCity.toUpperCase();
+  convertToGeoLoc(insensitiveCity)
+  };
+  const triggerError = (error) => {
+    setError(error);
+  };
+  const handleClick = (selectedCity) => {
+    setCity(selectedCity);
+  };
+  const handleMainClick = () => {
+    if (sideBarOpen) {
+      setSideBar(!sideBarOpen);
+    }
+  };
 
-  }
-  const handleClick = (selectedCity) => { setCity(selectedCity) }
-const handleMainClick=()=>{
-  if(sideBarOpen){ setSideBar(!sideBarOpen)}
-}
-console.log(favCollection)
   return (
-    <Container onClick={ handleMainClick} className= "main-container text-white">
+    <Container onClick={handleMainClick} className="main-container text-white">
+      <Row className="main-row">
+        <Col xs={12} md={9}>
+          <div className="search-fav-form">
+            {" "}
+            <form className=" search-form d-inline mt-3 text-white" onSubmit={submitForm}>
+              {error && temporaryCity != "" ? (
+                <div className="error-city">Please enter a valid city</div>
+              ) : (
+                ""
+              )}
+               <div id="dropDown-menu">TOKAT</div>
+              <div style={{ position: "relative" }} className="d-inline">
+                <input
+                  className="search-input p-1 m-1"
+                  placeholder="Search City"
+                  onChange={keyUp}
+                  value={temporaryCity}
+                />
+                <MdSearch className="search-icon" />
+              </div>
+            </form>
+            <div className="d-inline mt-3">
+              {" "}
+              <GiHamburgerMenu onClick={toggleSideBar} id="hamburger-menu" />
+              {city && !error && (
+                <div className="d-inline  ml-auto hearts">
+                  {favCollection &&
+                  favCollection.find((elem) => elem.favCity === city) ? (
+                    <MdFavorite
+                      className=" heart fav-heart ml-4"
+                      onClick={() => deleteFav(city)}
+                    />
+                  ) : (
+                    <MdFavoriteBorder
+                      className="heart ml-4"
+                      onClick={() => postFav()}
+                    />
+                  )}
+                </div>
+              )}
+            </div>
+          </div>
 
-
-      <Row  className="main-row">
-        <Col xs={12} md={9} >
-          <div className="search-fav-form"> <form className="d-inline mt-3 text-white"  onSubmit={submitForm}>
-      {error && temporaryCity!= "" ?  <div className="error-city">Please enter a valid city</div>:""}
-        <div style={{position:"relative"}} className="d-inline">  
-        <input
-          className="search-input p-1 m-1"
-
-          placeholder="Search City"
-          onChange={keyUp}
-          value={temporaryCity}
-       
-        />
-        <MdSearch className="search-icon"/>
- </div>
-   
-      </form>
-      <div className="d-inline mt-3">   <GiHamburgerMenu  onClick={toggleSideBar} id="hamburger-menu"/>
-         {city && !error &&
-        <div className="d-inline  ml-auto hearts">
-          {favCollection &&
-          favCollection.find((elem) => elem.favCity === city) ? (
-            <MdFavorite
-              className=" heart fav-heart ml-4"
-            
-              onClick={() => deleteFav(city)}
-            />
+          {geoCoo ? (
+            <CityApp triggerError={triggerError} geoCoo={geoCoo} />
           ) : (
-            <MdFavoriteBorder
-              className="heart ml-4"
-             
-              onClick={()=>postFav()}
-            />
+            ""
           )}
+        </Col>
 
-        </div>
-        }</div>
-     
-        </div>
-       
-         {city ? <CityApp triggerError={triggerError} city={city}/> : "" }
-    
-        </Col>
-        
         <Col md={3} className="shadow d-none d-md-block side-bar ">
-   
-      
-   
-    
-            <Row>{favCollection&&favCollection.map((fav)=><Favs key={fav._id} onClick={handleClick} deleteFavCity={deleteFav} city={fav} />)}</Row>
-   
+          <Row>
+            {favCollection &&
+              favCollection.map((fav) => (
+                <Favs
+                  key={fav._id}
+                  onClick={handleClick}
+                  deleteFavCity={deleteFav}
+                  city={fav}
+                />
+              ))}
+          </Row>
         </Col>
-      
       </Row>
 
-      {sideBarOpen? <div className="special-sideBar-wrapper"><Row className="special-sideBar">{favCollection&&favCollection.map((fav)=><Favs key={fav._id} onClick={handleClick} deleteFavCity={deleteFav} city={fav} />)}</Row></div>:""} 
+      {sideBarOpen ? (
+        <div className="special-sideBar-wrapper">
+          <Row className="special-sideBar">
+            {favCollection &&
+              favCollection.map((fav) => (
+                <Favs
+                  key={fav._id}
+                  onClick={handleClick}
+                  deleteFavCity={deleteFav}
+                  city={fav}
+                />
+              ))}
+          </Row>
+        </div>
+      ) : (
+        ""
+      )}
     </Container>
   );
 }
 
 export default Main;
-
-
