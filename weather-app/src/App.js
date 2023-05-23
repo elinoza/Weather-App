dc29d09b4f203c937c6fc1499e9c5c15a60cb5f2
elinoza <hillcakmak@gmail.com>
2023-05-23 21:29:55 +0300

    password reset e mail send implemented

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -8,6 +8,7 @@ import "./App.css";
 import Main from "./components/Main"
 import Login from "./components/auth/Login"
 import Register from "./components/auth/Register"
+import Reset from "./components/auth/Reset"
 import { ThemeContext,initialState } from './contexts/theme';
 
 function App() {
@@ -21,6 +22,7 @@ function App() {
 <Route exact path="/" component={Login} />
  <Route exact path="/Main" component={Main} />
  <Route exact path="/Register" component={Register} />
+ <Route exact path="/Reset" component={Reset} />
  </ThemeContext.Provider>
  </Router>
 
---
+++ weather-app/src/components/auth/Reset.jsx
@@ -0,0 +1,92 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container,Button } from "react-bootstrap";
+import {useState,useEffect} from 'react'
+import {useHistory} from 'react-router-dom'
+import { emailRegex } from "../../functions/functions";
+
+const Reset =()=>{
+
+
+   
+    const [password, setPassword] = useState("")
+  
+
+    const history = useHistory()
+    
+  
+const signUp=async (e)=>{
+e.preventDefault()
+
+try {
+    const url = process.env.REACT_APP_URL;
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    console.log(urlParams.get("accessToken"));
+    const token = urlParams.get("accessToken");
+    console.log("token",token);
+    let query = `/users/me`;
+    let response = await fetch(url + query ,
+      {
+        method: 'PUT',
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({password}),
+         withCredentials: true // use cookies
+      });
+    if (response.ok) {
+    
+      console.log("password is resetted by successfully", response);
+      history.push("/")
+      
+  
+    } else {
+      console.log(response);
+    }
+    
+} catch (error) {
+    console.log(error)
+}
+
+}
+
+
+
+    return (
+        <>
+
+    <Container 
+     id="signup-page-wrapper"
+     className="ThunderStorm shadow d-flex  align-items-center main-container justify-content-center "
+     style={{border: "none", padding:"20px"}}
+     >
+    
+      <div className="form-inputs">
+        <form className="form" onSubmit={signUp} novalidate>
+      
+          <label>Password</label>
+          <input
+            className="form-input"
+            id="password"
+            name="password"
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+            required
+          />
+          <br />
+  <input   className="form-input-submit align-self-center" type="submit" value="SIGN UP" />
+         
+      
+        </form>
+       
+        </div>
+    </Container>
+
+        </>
+    )
+}
+export default Reset
